38be9d136a3cfe7758047d035923563b912eca7b
shamsu711 <159604306+shamsu711@users.noreply.github.com>
2024-08-11 11:38:33 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2-instance" {
-  ami           = lookup(var.AMIS, var.AWS_REGION, "") # last parameter is the default value
+  ami           = "ami-0ba9883b710b05ac6" # last parameter is the default value
   instance_type = "t2.medium"
   disable_api_termination = true
 }
