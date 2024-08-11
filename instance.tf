e88e383177c797bdd73eccbffc5270cf171d6fcc
shamsu711 <159604306+shamsu711@users.noreply.github.com>
2024-08-11 10:33:23 +0530

    Add files via upload

---
+++ instance.tf
@@ -0,0 +1,7 @@
+resource "aws_instance" "ec2-instance" {
+  ami           = lookup(var.AMIS, var.AWS_REGION, "") # last parameter is the default value
+  instance_type = "t2.medium"
+  disable_api_termination = true
+  env = var.env
+}
+
---
+++ vars.tf
@@ -0,0 +1,22 @@
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-0ba9883b710b05ac6"
+    us-west-2 = "ami-06b94666"
+    eu-west-1 = "ami-0d729a60"
+  }
+}
+
+variables "env" {
+  default = ""
+}
